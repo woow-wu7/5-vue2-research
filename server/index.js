c788d9c95008eddccd3d88b610367176d76b782e
woow_wu7 <3107075636@.com>
2020-07-18 18:02:41 +0800

    login

--- server/index.js
+++ server/index.js
@@ -1,17 +1,34 @@
 // tslint:disable
- /* eslint-disable */ 
+/* eslint-disable */
 const express = require('express')
+var bodyParser = require('body-parser');
 
 const app = express()
 const port = 3000
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
 app.get('/', (req, res) => new Promise(resolve => {
   setTimeout(() => {
     resolve()
-  }, 10000)
+  }, 1000)
 }).then(() => res.json('11113')))
 
 
-// app.get('/', (req, res) => res.json('111'))
+app.post('/login', (req, res) => new Promise(resolve => {
+  setTimeout(() => {
+    console.log(req.body, 'req.body')
+    if (req.body && req.body.name === 'admin01') {
+      resolve()
+    }
+  }, 1000)})
+  .then(() => res.json({ token: 'token12345678' }))
+  .catch(err => console.log(err))
+)
 
+
+
+// app.get('/', (req, res) => res.json('111'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
